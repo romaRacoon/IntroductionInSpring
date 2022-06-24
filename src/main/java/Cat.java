675bb81a8bcc18ccdd533bcf7a39e58e77b9e9b9
romaRacoon <roma1808@inbox.ru>
2022-06-25 00:03:39 +0300

    Start make a task

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
